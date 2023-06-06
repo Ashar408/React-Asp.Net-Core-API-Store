11618965648c51e3b3c0b3e3513b70a1a90ea25c
Ashar Rasheed <asharrasheed32@gmail.com>
2023-06-07 01:17:37 +0500

    End Product Details section

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
-import { Product } from '../models/product';
 import Catalog from '../../features/catalog/Catalog';
 import { Container, CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Header from './Header';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from '../../features/home/Home';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/About';
+import ContactPage from '../../features/contact/Contact';
 
 
 
@@ -24,10 +28,15 @@ function handleThemeChange(){
     <ThemeProvider theme={theme}>
     <CssBaseline/>
       <Header darkMode={darkmode} handleThemeChange={handleThemeChange} />
-      <Container>
-     <Catalog/>
+    <Container>
+      <Routes>
+     <Route path='/' Component={HomePage} />
+     <Route path='/catalog' Component={Catalog} />
+     <Route path='/catalog/:id' Component={ProductDetails} />
+     <Route path='/about' Component={AboutPage} />
+     <Route path='/contact' Component={ContactPage} />
+     </Routes>
      </Container>
-      
     </ThemeProvider>
   )
 }
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,18 +1,81 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 interface Props{
     darkMode: boolean;
     handleThemeChange:()=>void;
 }
+const midLink=[
+    {title:'catalog', path: '/catalog'},
+    {title:'about', path: '/about'},
+    {title:'contact', path: '/contact'}
+]
+
+const rightLink=[
+    {title:'login', path: '/login'},
+    {title:'register', path: '/register'}
+]
+const navStyles={
+    color: 'inherit',
+    typography: 'h6',
+    textDecoration:'none',
+    '&:hover':{
+        color:'grey.500'
+    },
+    '&.active':{
+        color:'text.secondary'
+    }
+    }
+
 export default function Header({darkMode, handleThemeChange}:Props){
  
     return(
     
         <AppBar position='static' sx={{mb:3}}>
-        <Toolbar>
-            <Typography variant="h6">
+        <Toolbar sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+
+        <Box sx={{display:'flex', alignItems:'center'}}>
+        <Typography variant="h6" component={NavLink} to={'/'} sx={navStyles}>
                 Mark-Store
             </Typography>
             <Switch checked={darkMode} onChange={handleThemeChange}/>
+        </Box>
+
+        <Box>
+           <List sx={{display:'flex'}}>
+                {midLink.map(({title, path}) =>(
+                    <ListItem
+                    component={NavLink}
+                    to={path}
+                    key={path}
+                    sx={navStyles}    
+                    >
+                        {title.toUpperCase()}
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+
+        <Box sx={{display:'flex', alignItems:'center'}}> 
+            <IconButton size="large" sx={{color:'inherit'}}>
+                <Badge badgeContent={4} color="secondary">
+                    <ShoppingCart/>
+                </Badge>
+            </IconButton>
+            <List sx={{display:'flex'}}>
+                {rightLink.map(({title, path}) =>(
+                    <ListItem
+                    component={NavLink}
+                    to={path}
+                    key={path}
+                    sx={navStyles}    
+                    >
+                        {title.toUpperCase()}
+                    </ListItem>
+                ))}
+            </List>
+        </Box> 
         </Toolbar>
         </AppBar>
         
